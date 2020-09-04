d71c939cf586b0bb44ff49e6ec55c42011ca50fb
duochen13 <duochen@umich.edu>
2020-09-04 14:35:23 -0400

    add text box

--- front-end/src/components/TextBox.js
+++ front-end/src/components/TextBox.js
@@ -1,21 +1,13 @@
 
 import React from 'react';
 
-import { Input } from 'antd';
 
 class TextBox extends React.Component {
-
-    onChange = (e) => {
-        console.log("text changed: ", e);
-    }
 
     render() {
         return (
             <div>
                 This is a textbox
-                <Input >
-                    placeholder="Enter the username"
-                </Input>
             </div>
         )
     }
