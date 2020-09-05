7b6f30705a396f6f4092e88db1dc04725743f58b
zhuhongt <zhuhongt@umich.edu>
2020-09-04 20:13:57 -0400

    aggregate by emotion

--- back-end/utils.py
+++ back-end/utils.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import datetime
+from dateutil.parser import parse
 from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
@@ -7,11 +9,90 @@ APIKEY = "LHiAsPDfojrabmyLdVSbw87gY4hVJScdoIyRD7nNHKao"
 URL = "https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/a624be02-9f6a-4a57-8b1c-2fa687021e3b"
 VERSION = "4.6.0"
 
-def bar_chart(data):
+def aggregate_by_person(data):
+    for item in data:
+        print(data[])
     return NotImplemented
 
-def line_chart(data):
-    return NotImplemented
+
+def aggregate_by_tone(data):
+    """json_dict =
+    {
+        emotion_name:{
+            "now": {
+                "score": float,
+                "count": int,
+            },
+            "a_week_before": {
+                "score": float,
+                "count": int,
+            },
+            "most_recent_date": int,
+        }
+    }
+    """
+    now = datetime.date.today()
+    date_one = str(now.year)+'{0:0=2d}'.format(now.month)+'{0:0=2d}'.format(now.day)
+    one_week_ago = now - datetime.timedelta(days=7)
+    date_two = str(one_week_ago.year)+'{0:0=2d}'.format(one_week_ago.month)+'{0:0=2d}'.format(one_week_ago.day)
+    two_week_ago = now - datetime.timedelta(days=14)
+    date_three = str(two_week_ago.year)+'{0:0=2d}'.format(two_week_ago.month)+'{0:0=2d}'.format(two_week_ago.day)
+    recent_json_dict = {}
+    previous_json_dict = {}
+    most_recent = {}
+    for item in data:
+        tones = item['document_tone']['tones']
+        if int(item['date']) > int(date_two):
+            for tone in tones:
+                if tone['tone_name'] == '':
+                    continue
+                if tone['tone_name'] in previous_json_dict:
+                    recent_json_dict[tone['tone_name']]["score"] += tone['score']
+                    recent_json_dict[tone['tone_name']]["count"] += 1
+                else:
+                    recent_json_dict[tone['tone_name']] = {}
+                    recent_json_dict[tone['tone_name']]["score"] = tone['score']
+                    recent_json_dict[tone['tone_name']]["count"] = 1
+                if tone['tone_name'] not in most_recent:
+                    most_recent['tone_name'] = item['date']
+                elif tone['tone_name'] > most_recent['tone_name']:
+                    most_recent['tone_name'] = item['date']
+        elif int(item['date']) > int(date_three):
+            for tone in tones:
+                if tone['tone_name'] == '':
+                    continue
+                if tone['tone_name'] in previous_json_dict:
+                    previous_json_dict[tone['tone_name']]['score'] += tone['score']
+                    previous_json_dict[tone['tone_name']]['count'] += 1
+                else:
+                    previous_json_dict[tone['tone_name']] = {}
+                    previous_json_dict[tone['tone_name']]['score'] = tone['score']
+                    previous_json_dict[tone['tone_name']]['count'] = 1
+
+                if tone['tone_name'] not in most_recent:
+                    most_recent[tone['tone_name']] = item['date']
+                elif item['date'] > most_recent[tone['tone_name']]:
+                    most_recent[tone['tone_name']] = item['date']
+    json_dict = {}
+    for tone in previous_json_dict:
+        json_dict[tone] = {}
+        json_dict[tone]['a_week_before'] = {}
+        json_dict[tone]['a_week_before']['score'] = previous_json_dict[tone]['score'] / previous_json_dict[tone]['count']
+        json_dict[tone]['a_week_before']['count'] = previous_json_dict[tone]['count']
+        if tone in most_recent:
+            json_dict[tone]['most_recent_date'] = most_recent[tone]
+
+    for tone in recent_json_dict:
+        if tone not in json_dict:
+            json_dict[tone] = {}
+        json_dict[tone]['now'] = {}
+        json_dict[tone]['now']['score'] = recent_json_dict[tone]['score'] / recent_json_dict[tone]['count']
+        json_dict[tone]['now']['count'] = recent_json_dict[tone]['count']
+        if tone in most_recent and tone not in json_dict:
+            json_dict[tone]['most_recent_date'] = most_recent[tone]
+
+    print(json_dict)
+    return json_dict
 
 
 def aggregate_by_date(data):
@@ -65,7 +146,7 @@ def aggregate_by_date(data):
                         max_tone = tone
             json_dict[item]['tone_name'] = max_tone
             json_dict[item]['tone_score'] = max_score
-    print(json_dict)
+    return json_dict
 
 
 def main():
@@ -88,7 +169,7 @@ def main():
         response = requests.get(URL+'/v3/tone', params=params, auth=('apikey', APIKEY))
         response = json.loads(response.text)
         item['document_tone'] = response['document_tone']
-    aggregate_by_date(data)
+    aggregate_by_tone(data)
 
 
 
